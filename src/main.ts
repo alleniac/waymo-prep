0c8d8d4e06cd37a0556f3314d84511589e6f5271
allen <theallenma@gmail.com>
2026-07-04 14:42:39 -0700

    fix to the problems of the counter

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,3 @@
-/**
-a text displaying the current number.
-a button that increment that number.
-a button the decrease that number. 
-state would be: the current number, initial value is 0, displayed upon first render.
-a render function renders the HTML given the state.
-event listeners are: one listener for the increase button, one listener for the decrease button, handling the mutation of the number, and apply the new state by calling render function again.
-numbers can be negative.
- */
-
 type State = {
   count: number;
 };
@@ -18,15 +8,15 @@ let state: State = {
 
 const root = document.getElementById("app")!;
 
-function setState(next: State) {
+function setState(next: Partial<State>) {
   state = { ...state, ...next };
-  render();
+  update();
 }
 
-function render() {
+function mount() {
   root.innerHTML = `
     <button id="decrease-button">-</button>
-    <span>${state.count}</span>
+    <span id="count">${state.count}</span>
     <button id="increase-button">+</button>
   `;
   root
@@ -37,14 +27,9 @@ function render() {
     .addEventListener("click", () => setState({ count: state.count - 1 }));
 }
 
-render();
+function update() {
+  const countEl = document.getElementById("count")!;
+  countEl.textContent = String(state.count);
+}
 
-/**
-Problem with this version:
-
-1. innerHTML nuke every time the render() is called. This is harmless in this practice, but harmful in most of others.
-The whole HTML tree is demolished, including everything that was on the nodes, like all the event listeners, focus, input state, etc.  
-The right way is to build once, wire once, and update the exact node.
-
-2. setState should accept a Partial<State> to be safe.
- */
+mount();
